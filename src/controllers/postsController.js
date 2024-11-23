2f607377003043a4ab87bb2c53f7c6c36a2ae504
recpires <rec.pires7@gmail.com>
2024-11-22 23:29:13 -0300

    feat: add multer

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -15,3 +15,23 @@ export async function postarNovoPost(preq, res) {
     res.status(500).json({ Erro: "Falha na Requisição" });
   }
 }
+export async function postarNovoPost(preq, res) {
+  const novoPost = req.body;
+  try {
+    const postCriado = await criarPost(novoPost);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ "Erro": "Falha na Requisição" });
+  }
+}
+export async function uploadImagem(preq, res) {
+  const novoPost = req.body;
+  try {
+    const postCriado = await criarPost(novoPost);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha na Requisição" });
+  }
+}
